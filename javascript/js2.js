87d706f91d0517d735f13be29a64d37d68e92488
jongwoooo <doohee6310@gmail.com>
2024-07-08 19:42:19 +0900

    연산자 일부 공부

---
+++ javascript/js2.js
@@ -0,0 +1,13 @@
+console.log("Hello");
+console.log("Hello");
+
+var name;
+console.log(name);
+var name = "World";
+console.log(name);
+//호이스팅
+//변수 선언문이 코드의 최상단으로 끌어올려짐
+//할당은 실제 실행위치에서
+
+console.log(jongWoo);
+let jongWoo = "이종우";
